9c0abbc1488745d67f8b6b4b806f9028f4fb2de3
lbxx <476427808@qq.com>
2018-12-17 22:52:37 +0800

    临时提交

---
+++ src/main/java/com/kboss/threadstudy/p147/MyThread.java
@@ -0,0 +1,16 @@
+package com.kboss.threadstudy.p147;
+
+public class MyThread extends Thread {
+    private Object lock;
+
+    public MyThread(Object lock) {
+        super();
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        Service service = new Service();
+        service.testMethod(lock);
+    }
+}
