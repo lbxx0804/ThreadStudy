55c3ee98c8dc42f4773d6de79b832ba2c6853afd
lbxx <476427808@qq.com>
2018-12-23 15:15:08 +0800

    临时提交

---
+++ src/main/java/com/kboss/threadstudy/p167/ThreadP.java
@@ -0,0 +1,17 @@
+package com.kboss.threadstudy.p167;
+
+public class ThreadP extends Thread {
+    private P p;
+
+    public ThreadP(P p) {
+        super();
+        this.p = p;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            p.pushService();
+        }
+    }
+}
