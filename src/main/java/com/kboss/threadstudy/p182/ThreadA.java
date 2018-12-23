e08186159460e4f1d120ab40d24b357f4078fb2c
lbxx <476427808@qq.com>
2018-12-23 21:45:30 +0800

    临时提交

---
+++ src/main/java/com/kboss/threadstudy/p182/ThreadA.java
@@ -0,0 +1,11 @@
+package com.kboss.threadstudy.p182;
+
+public class ThreadA extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            String newString = new String();
+            //System.out.println(Math.random());
+        }
+    }
+}
