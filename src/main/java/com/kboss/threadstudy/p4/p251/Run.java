98ab89f4b7ee8ce44383538db1256d5795131a87
luwg <luwg@zzlc.com>
2019-02-12 17:48:32 +0800

    临时提交

--- src/main/java/com/kboss/threadstudy/p4/p251/Run.java
+++ src/main/java/com/kboss/threadstudy/p4/p251/Run.java
@@ -1,5 +1,7 @@
 package com.kboss.threadstudy.p4.p251;
 
+import java.util.*;
+
 public class Run {
     public static void main(String[] args) {
         Service service = new Service();
@@ -7,5 +9,7 @@ public class Run {
         ThreadB threadB = new ThreadB(service);
         threadA.start();
         threadB.start();
+
+        HashMap
     }
 }
